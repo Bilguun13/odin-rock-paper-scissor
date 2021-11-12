6ee57feb882b2fe1b4093cc695183336d76e1b94
Bilguun13 <83194126+Bilguun13@users.noreply.github.com>
2021-11-12 22:12:35 +0800

    second sub-problem solved, currently plays a single round of game

--- myscript.js
+++ myscript.js
@@ -13,10 +13,22 @@ function computerPlay() {
 }
 
 //2. Single round of game using above function and assuming to get user input
+function singleRound(computerSelection,userSelection) {
     //convert user input into lowercase
-
+    userSelection = userSelection.toLowerCase();
     //use if function to compare user and cpu choices, also declare winner
-
+    if (userSelection === computerSelection) {
+        return "Tie!"
+    }else if (userSelection == "rock" && computerSelection == "scissor"){
+        return `You win! Rock beats Paper`
+    }else if (userSelection == "paper" && computerSelection == "rock"){
+        return `You win! Paper beats Rock`
+    }else if (userSelection == "scissor" && computerSelection == "paper"){
+        return `You win! Scissor beats Paper`
+    }else{
+        return `You lose! ${computerSelection} beats ${userSelection}`
+    }
+}
 
 //3. Function for a 5 round of game, taking user input
     //loop for 5 round
