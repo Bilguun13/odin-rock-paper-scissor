e803f17bf59c6a69da793efa77fee2e61d615e00
Bilguun13 <83194126+Bilguun13@users.noreply.github.com>
2021-11-12 21:53:34 +0800

    pseudocode for the project

---
+++ myscript.js
@@ -0,0 +1,16 @@
+//1. Generate a cpu choice in a function called computerPlay
+    //generate a number between 0 and 2
+
+    //for corresponding number, return: 0 = rock, 1 = paper, 2 = scissor
+
+
+//2. Single round of game using above function and assuming to get user input
+    //convert user input into lowercase
+
+    //use if function to compare user and cpu choices, also declare winner
+
+
+//3. Function for a 5 round of game, taking user input
+    //loop for 5 round
+        //each round prompt user input
+        //and play singleRound
