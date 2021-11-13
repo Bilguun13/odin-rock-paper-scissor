61f12f920149a40974aa115553419d5428a55357
Bilguun13 <83194126+Bilguun13@users.noreply.github.com>
2021-11-13 23:05:56 +0800

    rock paper scissor project done :), currently keeps count of user and cpu and also declares a winner

--- myscript.js
+++ myscript.js
@@ -1,3 +1,5 @@
+let userCount = 0;
+let cpuCount = 0;
 //1. Generate a cpu choice in a function called computerPlay
 function computerPlay() {
     //generate a number between 0 and 2
@@ -20,23 +22,42 @@ function singleRound(computerSelection,userSelection) {
     if (userSelection === computerSelection) {
         return "Tie!"
     }else if (userSelection == "rock" && computerSelection == "scissor"){
+        userCount++
         return `You win! Rock beats Paper`
     }else if (userSelection == "paper" && computerSelection == "rock"){
+        userCount++
         return `You win! Paper beats Rock`
     }else if (userSelection == "scissor" && computerSelection == "paper"){
+        userCount++
         return `You win! Scissor beats Paper`
     }else{
+        cpuCount++
         return `You lose! ${computerSelection} beats ${userSelection}`
     }
 }
 
 //3. Function for a 5 round of game, taking user input
 function game() {
+    updateScore();
     //loop for 5 round
     for (let i = 0; i < 5; i++) {
         //each round prompt user input
         let getInput = prompt("Enter your pick: ").toLowerCase();
         //and play singleRound
-        console.log(singleRound(computerPlay(),getInput));
+        console.log("round " + (i + 1) + " : " + singleRound(computerPlay(),getInput));
+        console.log("User: " + userCount + " CPU: " + cpuCount)
     }
+    declareWinner();
+}
+
+//updating score with singleRound return;
+function updateScore() {
+    userCount = 0
+    cpuCount = 0;
+}
+
+function declareWinner() {
+    (userCount > cpuCount)? console.log("You win!") :
+    (userCount == cpuCount)? console.log("Tie") :
+    console.log("You lose :(")
 }
